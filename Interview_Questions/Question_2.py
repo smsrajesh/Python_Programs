73e2ac66dda9e97df1be5127e61bbdf7f952e308
smsrajesh <smsrajeshrangisetti369@gmail.com>
2025-12-09 16:52:33 +0530

    updated

--- Interview_Questions/Question_2.py
+++ Interview_Questions/Question_2.py
@@ -4,12 +4,16 @@
     output=abcd
 '''
 
+'''
+    text = "hello   world"
+    print(text.replace(" ", ""))
+'''
 
 def fun(a):
     result=""
     for i in a:
         if i not in  result:
             result+=i
-    return result
+    return result.replace(" ","")
 
-print(fun(input("Enter a string with duplicate letters without any gaps :")))
+print(fun(input("Enter a string with duplicate letters :")))

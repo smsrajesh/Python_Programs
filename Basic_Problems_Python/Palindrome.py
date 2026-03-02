ab49cfc518643434effda6e3d9e69fbf00c6c02d
smsrajesh <smsrajeshrangisetti369@gmail.com>
2026-03-02 11:03:11 +0530

    added optimised way

--- Basic_Problems_Python/Palindrome.py
+++ Basic_Problems_Python/Palindrome.py
@@ -19,3 +19,21 @@ else:
     print(f"{name} is not a palindrome")
 
 
+
+# Optimised way to check if a number is a palindrome without converting it to a string.
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        
+        if x % 10 == 0 and x != 0:
+            return False
+        
+        reversed_half = 0
+        
+        while x > reversed_half:
+            reversed_half = reversed_half * 10 + x % 10
+            x //= 10
+        
+        return x == reversed_half or x == reversed_half // 10
